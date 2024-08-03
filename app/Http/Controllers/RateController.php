fcbcf7fc7106a13e3c03967b4e195ae68b9db461
Ghina Alrefai <ghina.alrefai12@gmail.com>
2024-08-03 12:52:05 +0300

    new commit

--- app/Http/Controllers/RateController.php
+++ app/Http/Controllers/RateController.php
@@ -2,9 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Responses\Response;
+use App\Services\rateService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class RateController extends Controller
 {
-    //
+    protected $rateService;
+
+    public function __construct(RateService $rateService)
+    {
+        $this->rateService = $rateService;
+    }
+
+    public function addRate(Request $request, $exhibitionId): JsonResponse
+    {
+        $data=[];
+        try{
+            $data=$this->rateService->addRate($request, $exhibitionId);
+            return Response::Success($data['data'],$data['message']);
+        }catch (\Throwable $th){
+            $message=$th->getMessage();
+            return Response::Error($data,$message);
+        }
+    }
+
+    public function updateRate(Request $request, int $rateId): JsonResponse
+    {
+        $data=[];
+        try{
+            $data=$this->rateService->updateRate($request, $rateId);
+            return Response::Success($data['data'],$data['message']);
+        }catch (\Throwable $th){
+            $message=$th->getMessage();
+            return Response::Error($data,$message);
+        }
+    }
+
+    public function showExhibitionRate(int $exhibitionId): JsonResponse
+    {
+        $data=[];
+        try{
+            $data=$this->rateService->showExhibitionRate($exhibitionId);
+            return Response::Success($data['data'],$data['message']);
+        }catch (\Throwable $th){
+            $message=$th->getMessage();
+            return Response::Error($data,$message);
+        }
+    }
+
 }
