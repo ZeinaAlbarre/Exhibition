47570289257d2d05ee287f95a00e002ab3512be0
Ghina Alrefai <ghina.alrefai12@gmail.com>
2024-08-07 00:34:52 +0300

    commit

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -1,10 +1,35 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Services\NotificationService;
 use Illuminate\Http\Request;
+
+
+
 
 class NotificationController extends Controller
 {
-    //
+    protected $notificationService;
+
+    public function __construct(NotificationService $notificationService)
+    {
+        $this->notificationService = $notificationService;
+    }
+
+
+
+    public function send(Request $request)
+    {
+
+
+        $token=$request->input('token');
+        $title=$request->input('title');
+        $body=$request->input('body');
+        $data=$request->input('data',[]);
+        $response=$this->notificationService->send($token,$title,$body,$data);
+            return response()->json(['success' => true,$response]);
+
+    }
+
+
 }
